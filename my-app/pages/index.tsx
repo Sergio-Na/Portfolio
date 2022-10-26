89e93189ff927f12c73fe9af44f2866ddd5aa30e
JoJonas <sergionajm@gmail.com>
2022-10-25 21:37:04 -0400

    created Header component and 70% of Hero component

---
+++ my-app/pages/index.tsx
@@ -0,0 +1,28 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+export default function Home() {
+  return (
+    <div className="bg-[rgb(63,29,20)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+      <Head>
+        <title>Sergio's portfolio</title>
+      </Head>
+      <Header />
+
+      {/* Hero */}
+      <section id="hero" className="snap-center">
+        <Hero />
+      </section>
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skills */}
+
+      {/* Projects */}
+
+      {/* Contact me */}
+    </div>
+  );
+}
