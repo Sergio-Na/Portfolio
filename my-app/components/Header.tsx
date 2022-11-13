6f7524e69619b40f9496fddd7fb998f3a8423b8a
Ser1212 <sergionajm@gmail.com>
2022-11-13 16:59:45 -0500

    added resume in public

--- my-app/components/Header.tsx
+++ my-app/components/Header.tsx
@@ -62,7 +62,7 @@ export default function Header({}: Props) {
           type="button"
           className="text-gray-200 bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600 hover:bg-gradient-to-br shadow-lg shadow-[#FF731D]/50 dark:shadow-lg dark:shadow-orange-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
         >
-          Resume
+          <a href="Sergio_Resume.pdf">Resume</a>
         </button>
       </motion.div>
     </header>
